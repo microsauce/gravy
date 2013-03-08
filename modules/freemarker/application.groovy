482c59dc3f7c825436ae18077b5d89eb556d789a
microsauce <jboone@crunchbang.(none)>
2013-03-08 16:00:20 -0600

    format

--- modules/freemarker/application.groovy
+++ modules/freemarker/application.groovy
@@ -1,9 +1,8 @@
-
 import freemarker.cache.*
 import freemarker.template.*
 import java.util.concurrent.ConcurrentHashMap
 
-def documentRoot =  config.gravy.documentRoot ?: System.getProperty('gravy.viewRoot')
+def documentRoot = config.gravy.documentRoot ?: System.getProperty('gravy.viewRoot')
 def serviceUri = config.serviceUri ?: '/view/freemarker'
 def engines = new ConcurrentHashMap()
 
@@ -11,30 +10,29 @@ def engines = new ConcurrentHashMap()
 // lazy load a template engine instance for each module
 //
 def templateEngine = { moduleName ->
-	def engine = engines[moduleName]
-	if ( !engine ) {
-		// the second argument tells the loader to follow symlinks (necessary in dev mode)
-		def templateLoader = new FileTemplateLoader(new File(documentRoot+'/'+moduleName), true)
-		engine = new Configuration()
-		engine.setObjectWrapper(new DefaultObjectWrapper())
-		engine.setTemplateLoader(templateLoader)
+    def engine = engines[moduleName]
+    if (!engine) {
+        // the second argument tells the loader to follow symlinks (necessary in dev mode)
+        def templateLoader = new FileTemplateLoader(new File(documentRoot + '/' + moduleName), true)
+        engine = new Configuration()
+        engine.setObjectWrapper(new DefaultObjectWrapper())
+        engine.setTemplateLoader(templateLoader)
         engines[moduleName] = engine
-	}
-	engine
+    }
+    engine
 }
 
 route serviceUri, {
 
-	def model = req.get '_model'
-    def viewUri = req.getAttribute '_view' 	// not serialized
-    def module = req.getAttribute '_module'	// not serialized
+    def model = req.get '_model'
+    def viewUri = req.getAttribute '_view'     // not serialized
+    def module = req.getAttribute '_module'    // not serialized
     res.contentType = 'text/html'
 
     def template = templateEngine(module.name).getTemplate(viewUri)
-    if ( template )	{
+    if (template) {
         template.process(model, res.printer)
         res.printer.flush()
-    }
-    else throw new RuntimeException("Template not found for uri $viewUri")
+    } else throw new RuntimeException("Template not found for uri $viewUri")
 }
 
